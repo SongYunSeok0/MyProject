fd66edde3ad653606762bc6d556c5843ae1dd0d4
Song <qkfrus6623@naver.com>
2025-08-03 22:23:25 +0900

    폼 수정

--- sh_shop/forms.py
+++ sh_shop/forms.py
@@ -4,4 +4,20 @@ from sh_shop.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['type', 'gender','brand', 'price', 'size', 'content', 'uploaded_image']
+        fields = ['type', 'gender', 'brand', 'price', 'size', 'content', 'uploaded_image']
+        widgets = {
+            'price': forms.TextInput(attrs={
+                'id': 'id_price',
+                'placeholder': '가격을 입력하세요',
+                'autocomplete': 'off',
+            })
+        }
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if isinstance(price, str):
+            price = price.replace(',', '')
+        try:
+            return int(price)
+        except (ValueError, TypeError):
+            raise forms.ValidationError("숫자만 입력하세요.")
